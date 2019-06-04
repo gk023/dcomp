3a1498081adbc0ff7b40a0e13a3e81bca01ae7e7
Garrett King <kingg@pike.nscl.msu.edu>
2019-06-04 12:56:18 -0400

    updates to input format

--- mcmc/mcmc_run_dcomp.py
+++ mcmc/mcmc_run_dcomp.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python
+##!/user/bin/env python
 #
 # file: mcmc_run_dcomp.py 
 #
@@ -19,7 +19,6 @@ from subprocess import Popen, PIPE, STDOUT
 import numpy as np
 from scipy.stats import multivariate_normal
 import sys
-import multiprocessing as mp
 
 ##################### function definitions ########################
 
@@ -109,96 +108,16 @@ def update_parameters(parameters,step):
 	parameters = [str(x) for x in parameters]
 	return(parameters)
 
-
-def mcmc(parameter,exp,data_type,accept,thin,burn,scale):
-
-	saved=[] #store the values for the mcmc
-	xs_predictions=[] #store the cross sections
-	naccepted=0
-	nrejected=0 #keep track of acceptance and rejection in the run
-	
-	#Grab the parameters we're varying in the run
-	start=parameter[4:6]+parameter[9:14]
-	start=[float(x) for x in start] #make them floats
-	centers = np.asarray(start) #The means for the priors
-	
-	#Get data, likelihood, prior*likelihood combo for inital data set
-	
-	print("\nInitial Data Set:")
-	
-	start_th = dcomp(parameter,exp[:,0])
-	
-	start_like = likelihood(start_th,exp,data_type)[0]
-	
-	start_priorlike = start_like*prior(centers,centers,centers)
-	
-	
-	count=0
-	
-	
-	while(len(saved) < accept):
-	
-		count +=1 
-	
-		#Take a random step
-		step = take_step(start,scale)
-		new_list = update_parameters(parameter,step)[:20]
-	
-		#Repeat process to get prior*likelihood for new set
-		new = new_list[4:6]+new_list[9:14]
-		new = [float(x) for x in new]
-		new = np.asarray(new)
-		
-	        #print(new_list)		
-	
-		new_th = dcomp(new_list,exp_data[:,0])
-		new_like, new_chisquare = likelihood(new_th,exp_data,datatype)
-		new_priorlike = new_like*prior(centers,centers,new)
-	
-		ratio = new_priorlike/start_priorlike #ratio for Metroplois Hastings
-	        
-	    	#Use the Metropolis-Hastings Algorithm
-		if (ratio >= np.random.uniform()):
-			parameter = new_list
-			start = new
-			start_priorlike = new_priorlike
-			
-			#Only take sets after the burn in one every 'skip' acceptances
-			if((count > burn)):
-				naccepted += 1
-			
-				if(naccepted % thin == 0):	
-					saved.append(new.tolist()+[new_chisquare,new_like])
-	                        	xs_predictions.append(new_th[:,[0,datatype]].tolist())
-			
-	
-		elif(count > burn):
-			nrejected += 1
-	
-		if(count == burn):
-			print("BURN IN PERIOD ENDED")
-		if(count >= burn and len(saved) != accept):
-			print(str(len(saved))+" parameters accepted out of "+str(accept))
-			print("Run number: " + str(count+1))
-	
-		return(saved,xs_predictions,naccepted,nrejected)
-
-#def collect_result(result):
-#	global results
-#	results.append(result)
-
 #################################################################
-results=[]
-
 numerics=[]
 
 parameter_list = [] #list of dcomp parameters
 exp_data=[] #list for the experimental data
 
-all_saved=[]
-all_xs_predictions=[]
-#total_naccepted=0
-#total_nrejected=0
+saved=[]
+xs_predictions=[]
+naccepted=0
+nrejected=0
 
 cardfile = sys.argv[1]
 datatype = int(sys.argv[2])
@@ -246,66 +165,96 @@ max_accept = int(numerics[0])
 skip = float(numerics[1])
 burn_in = int(numerics[2])
 step_scale = float(numerics[3])
-nproc= int(numerics[4])
+
+print("\n\nPulling "+str(max_accept)+" parameters with:\nThinning: "+str(skip)+"\nBurn-in: "+str(burn_in)+"\nStep Scaling: "+str(step_scale))
+
+#Grab the parameters we're varying in the run
+start=parameter_list[4:6]+parameter_list[9:14]
+start=[float(x) for x in start] #make them floats
+centers = np.asarray(start) #The means for the priors
+
+#Get data, likelihood, prior*likelihood combo for inital data set
+
+print("Initial Data Set:")
+
+start_th = dcomp(parameter_list,exp_data[:,0])
+
+start_like = likelihood(start_th,exp_data,datatype)[0]
+
+start_priorlike = start_like*prior(centers,centers,centers)
 
 
-print("\n\nPulling "+str(max_accept*nproc)+" parameter sets on "+str(nproc)+" processors with:\nThinning: "+str(skip)+"\nBurn-in: "+str(burn_in)+"\nStep Scaling: "+str(step_scale))
+count=0
 
 
+while(len(saved) < max_accept):
 
-if __name__ == '__main__':
+	count +=1 
 
-	processes = []
+	#Take a random step
+	step = take_step(start,step_scale)
+	new_list = update_parameters(parameter_list,step)[:20]
 
-	for i in range(0,nproc-1):
-		p = mp.Process(target=mcmc, args=(parameter_list,exp_data,datatype,max_accept,skip,burn_in,step_scale,))
-		processes.append(p)
-		p.start()
+	#Repeat process to get prior*likelihood for new set
+	new = new_list[4:6]+new_list[9:14]
+	new = [float(x) for x in new]
+	new = np.asarray(new)
+	
+        #print(new_list)		
 
-	for process in processes:
-		process.join()
+	new_th = dcomp(new_list,exp_data[:,0])
+	new_like, new_chisquare = likelihood(new_th,exp_data,datatype)
+	new_priorlike = new_like*prior(centers,centers,new)
 
+	ratio = new_priorlike/start_priorlike #ratio for Metroplois Hastings
+        
+    	#Use the Metropolis-Hastings Algorithm
+	if (ratio >= np.random.uniform()):
+		parameter_list = new_list
+		start = new
+		start_priorlike = new_priorlike
+		
+		#Only take sets after the burn in one every 'skip' acceptances
+		if((count > burn_in)):
+			naccepted += 1
+		
+			if(naccepted % skip == 0):	
+				saved.append(new.tolist()+[new_chisquare,new_like])
+                        	xs_predictions.append(new_th[:,[0,datatype]].tolist())
+		
 
-	#p=mp.Pool(processes=nproc)
+	elif(count > burn_in):
+		nrejected += 1
 
-	#result = p.apply_async(mcmc,(parameter_list,exp_data,datatype,max_accept,skip,burn_in,step_scale,))
-	#print(result.get(timeout=9999999))
+	if(count == burn_in):
+		print("BURN IN PERIOD ENDED")
+	if(count >= burn_in and len(saved) != max_accept):
+		print(str(len(saved))+" parameters accepted out of "+str(max_accept))
+		print("Run number: " + str(count+1))
 
-#p.close()
-#p.join()
-
-#results.sort(key=lambda x: x[0])
-#results_final = [r for i, r in results]
-
-
-#print(results)
-
-#all_saved, all_xs_prediction, accepted, rejected = mcmc(parameter_list,exp_data,datatype,max_accept,skip,burn_in,step_scale)
-
-
-#print("\n\n\n% Accepted: "+str(100.*(float(accepted)/float(accepted+rejected-burn_in))))
-#print("% Rejected: "+str(100.*(float(rejected)/float(accepted+rejected-burn_in))))
+print("\n\n\n% Accepted: "+str(100.*(float(naccepted)/float(count-burn_in))))
+print("% Rejected: "+str(100.*(float(nrejected)/float(count-burn_in))))
 
 #print(saved)
 	
 
-#g=open("parameters.txt","w")
+g=open("parameters.txt","w")
 
-#for line in all_saved:
-#	for i in range(len(line)):
-#		g.write(str(line[i])+"   ")
-#	g.write("\n")
-#g.close()	
+for line in saved:
+	for i in range(len(line)):
+		g.write(str(line[i])+"   ")
+	g.write("\n")
+g.close()	
 
 
-#g=open("xs_bands.txt","w")
+g=open("xs_bands.txt","w")
 
-#for line in all_xs_predictions:
-#	for i in range(len(line)):
-#		for j in range(len(line[i])):
-#			g.write(str(line[i][j])+"   ")
-#	g.write("\n&\n")
-#g.close()	
+for line in xs_predictions:
+	for i in range(len(line)):
+		for j in range(len(line[i])):
+			g.write(str(line[i][j])+"   ")
+	g.write("\n&\n")
+g.close()	
 	
 
 
